df9b43cd5c3f608d9be9893d60ec7682e5d4cc5e
OrestisMakris <oamakris@gmail.com>
2025-05-19 14:48:59 +0000

    some delets of unused emb and cnn models

--- colbert_run/test_size.py
+++ colbert_run/test_size.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fae0fd96815056feb70214700317d738d992c90d90afd538d10c772c3167e235
-size 220
+oid sha256:6294a30f8ebfdd9af30e4977fd6a982a1c2144684b20a140cdf5a2714d3d6f8a
+size 215
