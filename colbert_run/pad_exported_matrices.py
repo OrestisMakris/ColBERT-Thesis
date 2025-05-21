f8f601eb1c090285c23922dd457150a969bd05a8
OrestisMakris <oamakris@gmail.com>
2025-05-21 16:03:04 +0000

    Scrypt that pads the sim scores all the exported pt similarity metrices (images)

--- colbert_run/pad_exported_matrices.py
+++ colbert_run/pad_exported_matrices.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae7586036644884689338184cc2d89929c57fa62ff030c0f7ee92903df68a7ef
-size 10307
+oid sha256:a7bae7f6aab5fa64690bde4d86abedf00d591bb8cabecb5a222ebfaf3c4aec8b
+size 8908
