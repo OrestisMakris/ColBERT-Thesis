955046b99a584217e46b5d561b631c80d43b27e2
OrestisMakris <oamakris@gmail.com>
2025-05-29 20:24:52 +0000

    Update model and training files; adjust file sizes and add new CNN classifier model

--- colbert_run/model_cnn.py
+++ colbert_run/model_cnn.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5fafa3d1ec9ae88df9c6f26fae260b80c1bb1e0c024177716d30b713ac93945f
-size 4549
+oid sha256:448c944cf263f69933ae07a7a92992cc78a6505ac4551d0f6be1c1297bcbff89
+size 4563
--- colbert_run/train_cnn.py
+++ colbert_run/train_cnn.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cbbd3bac5af98b516374011369c0e50fe8f7226088063b0dee9c09a31afac8be
-size 3445
+oid sha256:27f142e8f89805b2ddcfd5ae012cc4fcc7124477a5900f84f57f8c67d9f5ef3c
+size 3448
