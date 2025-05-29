eb94069c187a96179150d1a19c7db111401ad385
OrestisMakris <oamakris@gmail.com>
2025-05-29 11:21:57 +0000

    eval script

--- colbert_run/eval.py
+++ colbert_run/eval.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b047bf1aec55235f3ad21f0358ae75af75cf0f2458a1ec553c73b5a32aa06eaf
-size 600
+oid sha256:e9a14d1bebd0dbd66cdeb67318bfd50a133b9a4b99581fbb8fb0aaa98437962b
+size 1900
