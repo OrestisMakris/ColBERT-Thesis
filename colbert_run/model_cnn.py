c6fe16a897bf026bcf546b88daa6d7023c116a17
OrestisMakris <oamakris@gmail.com>
2025-06-02 12:07:40 +0000

    some changes

--- colbert_run/model_cnn.py
+++ colbert_run/model_cnn.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8967b0afda32e877eb2f674ca07e38ef964beaa99dd78d8233b84948aa0d4f97
-size 4620
+oid sha256:7f9898018ca47ef7b3b7acc69437158f2e2857dff81fcb8fa15e96670f1cfd99
+size 4728
--- colbert_run/train_cnn.py
+++ colbert_run/train_cnn.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e568e2fad2c02f7b2ec2a3def0689ab247d76b80917a7e00c61f1df69988b22d
-size 9704
+oid sha256:ba148f30364211b622a1ec001f303bb0fae3bed753f2be90677fae2aa4c713ce
+size 9714
