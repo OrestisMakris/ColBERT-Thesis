798ade0c0be0fa8d7bd3c641afd7f3f2d37389f7
OrestisMakris <oamakris@gmail.com>
2025-06-05 00:21:48 +0000

    Update indexer and retriever configurations for CF2 experiment
    - Changed the number of ranks in indexer.py from 4 to 1 and set nbits to None.
    - Updated experiment name in retriever.py from CF6 to CF2 and adjusted the search index accordingly.
    - Increased the number of results returned in the search from 1 to 100.
    - Removed commented-out code for clarity and maintainability.
    - Ensured consistency in experiment naming across indexer and retriever scripts.

--- thesis/colbert_run/indexer.py
+++ thesis/colbert_run/indexer.py
@@ -8,9 +8,9 @@ from colbert.infra import Run, RunConfig, ColBERTConfig
 from colbert import Indexer
 
 if __name__=='__main__':
-    with Run().context(RunConfig(nranks=4, experiment="CF2")):
+    with Run().context(RunConfig(nranks=1, experiment="CF2")):
         config = ColBERTConfig(
-            nbits=1,
+            nbits=None,
         )
         indexer = Indexer(checkpoint="./experiments/default/none/2025-06/04/13.17.54/checkpoints/colbert", config=config)
         indexer.index(name="CF2", collection="./dbpedia-entity_colbert_format/docs.tsv", overwrite=True)
